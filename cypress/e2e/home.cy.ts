13f55de7793c3c3006dba384511d45c452696790
Matilda Mared <matildamared@live.se>
2023-01-27 08:34:41 +0100

    add tests for home page

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("home page", () => {
+  it("the h1 contains the correct text", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("[data-test='hero-heading']")
+      .should("exist")
+      .contains("Testing Next.js Applications with Cypress")
+  })
+
+  it.only("the features on the home page are correct", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(0).contains(/4 courses/i)
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains(/free and open source/i)
   })
 })
