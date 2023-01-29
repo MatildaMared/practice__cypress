7606c3ee6060243abe8ac8bdc72ba41b1b7fa797
Matilda Mared <matildamared@live.se>
2023-01-29 06:58:11 +0100

    add tests for subscribe form

---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,26 @@
+describe("Newsletter Subscribe Form", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
+  it("allows users to subscribe to the email list", () => {
+    cy.getByData("email-input").type("matilda@mail.com")
+    cy.getByData("submit-button").click()
+    cy.getByData("success-message").should("exist").contains("matilda@mail.com")
+  })
+
+  it("does not allow a user to subscribe with an invalid email", () => {
+    cy.getByData("email-input").type("lalala")
+    cy.getByData("submit-button").click()
+    cy.getByData("success-message").should("not.exist")
+  })
+
+  it("does not allow a user that is already subscribed to subscribe again", () => {
+    const emailThatIsAlreadySubscribed = "john@example.com"
+    cy.getByData("email-input").type(emailThatIsAlreadySubscribed)
+    cy.getByData("submit-button").click()
+    cy.getByData("server-error-message")
+      .should("exist")
+      .contains(`${emailThatIsAlreadySubscribed} already exists`)
+  })
+})
