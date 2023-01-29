ca7ebe79352457a197fd5eb94eec9e38e8624a61
Matilda Mared <matildamared@live.se>
2023-01-29 07:06:02 +0100

    wrap context around hero section tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,19 +3,21 @@ describe("home page", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("the h1 contains the correct text", () => {
-    cy.get("[data-test='hero-heading']")
-      .should("exist")
-      .contains("Testing Next.js Applications with Cypress")
-  })
+  context("hero section", () => {
+    it("the h1 contains the correct text", () => {
+      cy.get("[data-test='hero-heading']")
+        .should("exist")
+        .contains("Testing Next.js Applications with Cypress")
+    })
 
-  it.only("the features on the home page are correct", () => {
-    cy.get("dt")
-      .eq(0)
-      .contains(/4 courses/i)
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt")
-      .eq(2)
-      .contains(/free and open source/i)
+    it.only("the features on the home page are correct", () => {
+      cy.get("dt")
+        .eq(0)
+        .contains(/4 courses/i)
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt")
+        .eq(2)
+        .contains(/free and open source/i)
+    })
   })
 })
--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -2,7 +2,6 @@ describe("Newsletter Subscribe Form", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000")
   })
-
   it("allows users to subscribe to the email list", () => {
     cy.getByData("email-input").type("matilda@mail.com")
     cy.getByData("submit-button").click()
